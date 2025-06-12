0977af5e9a14c43da73901abec136a9939853625
tien45 <130037376+tien45@users.noreply.github.com>
2025-06-12 22:22:14 +0700

    Delete tmindmap directory

--- tmindmap/src/authState.js
+++
@@ -1,8 +0,0 @@
-// src/composables/authState.js
-import { reactive } from 'vue';
-
-export const authState = reactive({
-    token: localStorage.getItem('token') || '',
-    userName: localStorage.getItem('userName') || '',
-    userIcon: localStorage.getItem('userIcon') || ''
-});
